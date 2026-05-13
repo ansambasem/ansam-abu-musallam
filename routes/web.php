a75654e4a74f62d9af1e70df1dd1780b6e4ea3c7
ansambasem <basmansam@gmail.com>
2026-05-13 09:03:32 +0300

    Routing and passing data from controller to view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\Factory;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(uri: '/about' , action: function(): Factory|View{
+$name ='ansam';
+
+$departments =[
+'01' =>'Tichnical',
+'2' => 'financial',
+'3' => 'Sales'
+
+];
+     //return view( 'about' , ['name' => $name]);
+    //return view( view:'about')->with( key: 'name', value : $name);
+    return view( view:'about' , data: compact('name' , 'departments'));
+
+});
+Route::post(uri: '/about' , action: function(): Factory|View{
+
+$name =$_POST['name'];
+$departments =[
+'01' =>'Tichnical',
+'2' => 'financial',
+'3' => 'Sales'
+
+];
+  return view( view:'about' , data: compact('name'));
+
+ //return view( view:'about');
+
+});
+
