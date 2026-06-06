e5093ca0df19a036ecd6f7b49bd5f2e13bfe812f
ansambasem <basmansam@gmail.com>
2026-06-06 09:40:05 +0300

    Eloquent Model

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Task;
 use Faker\Factory;
 use Illuminate\Http\Request;
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Http\Request;
